8821de31a95a0293b9abd4cbb6f7a3a51400ec4a
vnp23 <vnp23@imperial.ac.uk>
2024-02-16 15:08:45 +0000

    Added DB from WhereTo. Using the same DB, can change the tables.

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
 
+from db.DBClass import BorgDB
+
 app = Flask(__name__)
 
+
+dbConnection = BorgDB()
+
+
+def test_db_connection():
+    try:
+        dbConnection.get_connection()
+        print("DB connected")
+    except Exception as e:
+        print(e)
+        print("DB connection failed.")
+
+
+test_db_connection()
 
 @app.route('/')
 def hello_world():  # put application's code here
