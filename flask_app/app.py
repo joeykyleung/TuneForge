655be5268ee3edc8f0127cb02400214c08d1acc6
vnp23 <vnp23@imperial.ac.uk>
2024-03-05 14:52:52 +0000

    added loggers.

--- flask_app/app.py
+++ flask_app/app.py
@@ -52,9 +52,12 @@ def json_to_midi():
     data = request.get_json()
     notes_converter_url = fetch_microservice(Microservices.NOTES_CONVERTER.value)
     notes_converter_endpoint = notes_converter_url + '/'
+    app.logger.info("Sending request to " + notes_converter_endpoint + " with data: " + data)
     response = requests.post(notes_converter_endpoint, json=data)
     if response.status_code == '201':
-        #toDo: download
+        #toDo: download wav
+        #toDo: send to mood service and stuff
+        app.logger.info("Successfully pinged notes converter")
         pass
     else:
         #toDo: error
